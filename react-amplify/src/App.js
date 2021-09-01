92333c726b75d627bafc6533af81a74d5b476529
unknown <marcielbp@gmail.com>
2021-08-31 23:51:40 -0300

    #5 profile topbar and colors

--- react-amplify/src/App.js
+++ react-amplify/src/App.js
@@ -1,20 +1,35 @@
 import logo from './logo.svg';
 import React, { useEffect } from 'react'
 import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react'
+import { withAuthenticator, AmplifyGreetings, AmplifySignOut  } from '@aws-amplify/ui-react'
 import Webpages from './pages';
 import MapContainer from "google-maps-react";
 import { Auth } from 'aws-amplify'
 
+const userProfileData = {
+    username: 'undefined'
+};
+
+async function signOut() {
+    try {
+        await Auth.signOut();
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+}
+
+Auth.currentAuthenticatedUser().then(user => {userProfileData.username = user.username }).catch(err => console.log(err));
+
 function App() {
   return (
     <div className="App">
-    <div id=""
-      <h1>Patrol Tracker</h1>
+    <div id="profile-header"><h1>Patrol Tracker</h1></div>
+     <AmplifyGreetings username={userProfileData.username}></AmplifyGreetings>
       <div>
-      <a href="./profile">Profile</a>
-      <a href="./checkin">CheckIn</a>
-      <a href="./report">Report</a>
+      <a href="../profile">Profile</a>
+      <a href="../checkin">CheckIn</a>
+      <a href="../report">Report</a>
+     
         <Webpages />
       </div>
     </div>
