8494ee3acce8fee06f8ab051c0b1c0b087e0b37f
unknown <marcielbp@gmail.com>
2021-09-01 23:52:59 -0300

    #5 update profile topbar and colors

--- react-amplify/src/pages/index.js
+++ react-amplify/src/pages/index.js
@@ -10,12 +10,14 @@ import {
 import Profile from './profile';
 import Ronda from './ronda';
 import CheckIn from './checkin';
+import Report from './report';
 const Webpages = () => {
     return(
         <Router>
             <Route exact path="/profile" component= {Profile} />
             <Route path = "/ronda" component = {Ronda} />
             <Route path = "/checkin" component = {CheckIn} />
+            <Route path = "/report" component = {Report} />
         </Router>
     );
 };
